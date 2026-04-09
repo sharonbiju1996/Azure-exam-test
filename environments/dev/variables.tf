6dd9143830b2a0ced4fd77e5869c3f9f0890094d
sharonbiju1996 <62370160+sharonbiju1996@users.noreply.github.com>
2026-04-09 08:40:39 +0530

    Update variables.tf

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -24,6 +24,13 @@ variable "subnets" {
   }))
 }
 
+# ---------- VM VARIABLES ----------
+
+variable "vm_name" {
+  description = "Name of the VM"
+  type        = string
+}
+
 variable "vm_size" {
   description = "Azure VM size"
   type        = string
@@ -38,6 +45,63 @@ variable "admin_ssh_public_key" {
   type      = string
   sensitive = true
 }
+
+variable "disable_password_authentication" {
+  type    = bool
+  default = true
+}
+
+# ---------- NETWORK ----------
+
+variable "public_ip_name" {
+  type = string
+}
+
+variable "network_interface_name" {
+  type = string
+}
+
+variable "vm_subnet_key" {
+  description = "Subnet key for VM"
+  type        = string
+  default     = "vm"
+}
+
+# ---------- DISK ----------
+
+variable "os_disk_caching" {
+  type    = string
+  default = "ReadWrite"
+}
+
+variable "os_disk_storage_account_type" {
+  type    = string
+  default = "Standard_LRS"
+}
+
+# ---------- IMAGE ----------
+
+variable "vm_image_publisher" {
+  type    = string
+  default = "Canonical"
+}
+
+variable "vm_image_offer" {
+  type    = string
+  default = "0001-com-ubuntu-server-jammy"
+}
+
+variable "vm_image_sku" {
+  type    = string
+  default = "22_04-lts-gen2"
+}
+
+variable "vm_image_version" {
+  type    = string
+  default = "latest"
+}
+
+# ---------- TAGS ----------
 
 variable "tags" {
   type    = map(string)
