d33eeb8a23c062a05018491acd10538666d4bca3
sharonbiju1996 <62370160+sharonbiju1996@users.noreply.github.com>
2026-04-09 08:41:17 +0530

    Update vm.tf

--- environments/dev/vm.tf
+++ environments/dev/vm.tf
@@ -2,8 +2,8 @@ resource "azurerm_public_ip" "vm" {
   name                = var.public_ip_name
   location            = var.location
   resource_group_name = azurerm_resource_group.this.name
-  allocation_method   = var.public_ip_allocation_method
-  sku                 = var.public_ip_sku
+  allocation_method   = "Static"
+  sku                 = "Standard"
   tags                = local.common_tags
 }
 
@@ -14,9 +14,9 @@ resource "azurerm_network_interface" "vm" {
   tags                = local.common_tags
 
   ip_configuration {
-    name                          = var.nic_ip_configuration_name
+    name                          = "internal"
     subnet_id                     = module.vnet.subnet_ids[var.vm_subnet_key]
-    private_ip_address_allocation = var.private_ip_address_allocation
+    private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.vm.id
   }
 }
@@ -27,9 +27,11 @@ resource "azurerm_linux_virtual_machine" "vm" {
   location            = var.location
   size                = var.vm_size
   admin_username      = var.admin_username
+
   network_interface_ids = [
     azurerm_network_interface.vm.id
   ]
+
   disable_password_authentication = var.disable_password_authentication
   tags                            = local.common_tags
 
