9ff0d14282a7fcafe6bf5e631d1edcc07dba8b46
sharonbiju1996 <62370160+sharonbiju1996@users.noreply.github.com>
2026-04-09 10:42:48 +0530

    Update main.tf

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -19,21 +19,7 @@ module "vnet" {
   address_space       = var.address_space
   subnets             = var.subnets
 
-  nsg_rules = {
-    "nsg-pella-prod-eastus-vm" = [
-      {
-        name                       = "allow-ssh-internal"
-        priority                   = 100
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "22"
-        source_address_prefix      = "10.0.0.0/8"
-        destination_address_prefix = "*"
-      }
-    ]
-  }
+ 
 
   tags = local.common_tags
 }
