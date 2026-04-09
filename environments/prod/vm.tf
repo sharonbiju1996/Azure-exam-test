8b3eb242c7f6584204a4099d264064f593ac970d
sharonbiju1996 <62370160+sharonbiju1996@users.noreply.github.com>
2026-04-09 10:18:44 +0530

    Update vm.tf

--- environments/prod/vm.tf
+++ environments/prod/vm.tf
@@ -1,44 +1,22 @@
-resource "azurerm_network_interface" "vm" {
-  name                = "nic-${local.name_prefix}-vm"
+resource "azurerm_public_ip" "bastion" {
+  name                = var.bastion_public_ip_name
   location            = var.location
   resource_group_name = azurerm_resource_group.this.name
+  allocation_method   = var.bastion_public_ip_allocation_method
+  sku                 = var.bastion_public_ip_sku
+  tags                = local.common_tags
+}
+
+resource "azurerm_bastion_host" "this" {
+  name                = var.bastion_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.this.name
+  sku                 = var.bastion_sku
   tags                = local.common_tags
 
   ip_configuration {
-    name                          = var.nic_ip_config_name
-    subnet_id                     = module.vnet.subnet_ids[var.vm_subnet_key]
-    private_ip_address_allocation = var.private_ip_allocation_method
-  }
-}
-
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "vm-${local.name_prefix}"
-  resource_group_name = azurerm_resource_group.this.name
-  location            = var.location
-  size                = var.vm_size
-  admin_username      = var.admin_username
-
-  network_interface_ids = [
-    azurerm_network_interface.vm.id
-  ]
-
-  disable_password_authentication = var.disable_password_authentication
-  tags                            = local.common_tags
-
-  admin_ssh_key {
-    username   = var.admin_username
-    public_key = var.admin_ssh_public_key
-  }
-
-  os_disk {
-    caching              = var.os_disk_caching
-    storage_account_type = var.os_disk_storage_account_type
-  }
-
-  source_image_reference {
-    publisher = var.vm_image_publisher
-    offer     = var.vm_image_offer
-    sku       = var.vm_image_sku
-    version   = var.vm_image_version
+    name                 = var.bastion_ip_configuration_name
+    subnet_id            = module.vnet.subnet_ids[var.bastion_subnet_key]
+    public_ip_address_id = azurerm_public_ip.bastion.id
   }
 }
